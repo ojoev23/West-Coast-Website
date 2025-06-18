43db10a07e9a4a0e2d64e5056a823c6726d91cb8
ojoev23 <99378277+ojoev23@users.noreply.github.com>
2025-06-18 10:53:33 -0600

    feat: created the project through intellij

---
+++ src/main/java/com/joeolapurath/westcoaststorewebsite/WestCoastStoreWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.joeolapurath.westcoaststorewebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WestCoastStoreWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WestCoastStoreWebsiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/joeolapurath/westcoaststorewebsite/WestCoastStoreWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joeolapurath.westcoaststorewebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WestCoastStoreWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
